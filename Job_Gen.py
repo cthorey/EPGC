f0eb333c97dec7598ba960fc53cc5af8f3a6912a
Cthorey_Malbec <clement.thorey@gmail.com>
2015-02-11 18:38:58 +0100

    update

--- Job_Gen.py
+++ Job_Gen.py
@@ -47,12 +47,12 @@ Dict_Param = {'Sigma': ['5D-2'],
               'Grav': ['0D0'],
               'El': ['1D0'],
               'Nu': ['1D0','1D-2','1D-3'],
-              'Pe': ['1D0','1D-1','1D-2','1D-3','1D-4'],
-              'Psi': ['0.D0','1D0','5D0'],
+              'Pe': ['1D0','1D-1','1D-2','1D-3'],
+              'Psi': ['0D0','1D0','5D0'],
               'N1' : ['1D5'],
               'Dr' : ['1D-2'],
               'Ep': ['1D-4'],
-              'Dt' : ['1D-6']}
+              'Dt' : ['1D-6','1D-7']}
 
 M_grid = 4000
 Init = 0 # 1 If you want to begin for the last backup
