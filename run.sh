c587a35a3fa9832a03e23639510b97951a4a764b
cthorey <clement.thorey@gmail.com>
2015-02-25 18:49:24 +0100

    pathc run.sh

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ Compilateur -o Module_Thickness_GFD.o -c Module_Thickness_GFD.f90
 Compilateur -o Module_Thickness_Newton.o -c Module_Thickness_Newton.f90
 Compilateur -o Module_Thickness.o -c Module_Thickness.f90
 Compilateur -o main.o -c main.f90
-Compilateur -o G1 main.o Module_Complementaire.o Module_Conservation.o Module_Initialisation.o Module_Numerical_Integration.o Module_Output.o Module_Surface.o Module_Thermal.o Module_Thermal_GFD.o Module_Thermal_Newton.o Module_Thermal_Newton_Integration_Epaisseur.o Module_Thermal_Newton_OLD.o Module_Thickness.o Module_Thickness_GFD.o Module_Thickness_Newton.o
+Compilateur -o run main.o Module_Complementaire.o Module_Conservation.o Module_Initialisation.o Module_Numerical_Integration.o Module_Output.o Module_Surface.o Module_Thermal.o Module_Thermal_GFD.o Module_Thermal_Newton.o Module_Thermal_Newton_Integration_Epaisseur.o Module_Thermal_Newton_OLD.o Module_Thickness.o Module_Thickness_GFD.o Module_Thickness_Newton.o
 rm *.o
 rm *.mod
 
