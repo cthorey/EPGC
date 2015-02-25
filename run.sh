0dc495f0f27cbd60587d1ea2520797066f585de8
Cthorey_Malbec <clement.thorey@gmail.com>
2015-02-25 19:26:21 +0100

    update

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")"
 Compilateur -o Module_Numerical_Integration.o -c Module_Numerical_Integration.f90
 Compilateur -o Module_Complementaire.o -c Module_Complementaire.f90
 Compilateur -o Module_Conservation.o -c Module_Conservation.f90
-Compilateur -o Module_Initialisation.o -c Module_Initialisation.f90
+Compilateur -o Module_Init_tmp.o -c Module_Init_tmp.f90
 Compilateur -o Module_Output.o -c Module_Output.f90
 Compilateur -o Module_Surface.o -c Module_Surface.f90
 Compilateur -o Module_Thermal_GFD.o -c Module_Thermal_GFD.f90
@@ -15,7 +15,7 @@ Compilateur -o Module_Thickness_GFD.o -c Module_Thickness_GFD.f90
 Compilateur -o Module_Thickness_Newton.o -c Module_Thickness_Newton.f90
 Compilateur -o Module_Thickness.o -c Module_Thickness.f90
 Compilateur -o main.o -c main.f90
-Compilateur -o run main.o Module_Complementaire.o Module_Conservation.o Module_Initialisation.o Module_Numerical_Integration.o Module_Output.o Module_Surface.o Module_Thermal.o Module_Thermal_GFD.o Module_Thermal_Newton.o Module_Thermal_Newton_Integration_Epaisseur.o Module_Thermal_Newton_OLD.o Module_Thickness.o Module_Thickness_GFD.o Module_Thickness_Newton.o
+Compilateur -o run main.o Module_Complementaire.o Module_Conservation.o Module_Init_tmp.o Module_Numerical_Integration.o Module_Output.o Module_Surface.o Module_Thermal.o Module_Thermal_GFD.o Module_Thermal_Newton.o Module_Thermal_Newton_Integration_Epaisseur.o Module_Thermal_Newton_OLD.o Module_Thickness.o Module_Thickness_GFD.o Module_Thickness_Newton.o
 rm *.o
 rm *.mod
 
