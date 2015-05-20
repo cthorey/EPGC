35192a7014937f50852a5de187549160c1daf658
cthorey <clement.thorey@gmail.com>
2015-05-20 14:55:05 +0200

    add libarrya to run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+Compilateur -o lib_array.o -c lib_array.f90
 Compilateur -o Module_Numerical_Integration.o -c Module_Numerical_Integration.f90
 Compilateur -o Module_Complementaire.o -c Module_Complementaire.f90
 Compilateur -o Module_Conservation.o -c Module_Conservation.f90
@@ -18,7 +19,7 @@ Compilateur -o Module_Thickness_Skin_Newton_Roscoe.o -c Module_Thickness_Skin_Ne
 Compilateur -o Module_Thickness.o -c Module_Thickness.f90
 Compilateur -o main.o -c main.f90
 Compilateur -o Module_Surface.o -c Module_Surface.f90
-Compilateur -o run main.o Module_Complementaire.o Module_Conservation.o Module_Init_tmp.o Module_Numerical_Integration.o Module_Output.o Module_Surface.o Module_Thermal.o Module_Thermal_IntE_Newton_Bercovici.o Module_Thermal_Skin_GFD_Bercovici.o Module_Thermal_Skin_Newton_Arrhenius.o Module_Thermal_Skin_Newton_Bercovici.o Module_Thermal_Skin_Newton_Roscoe.o Module_Thickness.o Module_Thickness_Inte_GFD_Bercovici.o Module_Thickness_Skin_Newton_Arrhenius.o Module_Thickness_Skin_Newton_Bercovici.o Module_Thickness_Skin_Newton_Roscoe.o
+Compilateur -o run lib_array.o main.o Module_Complementaire.o Module_Conservation.o Module_Init_tmp.o Module_Numerical_Integration.o Module_Output.o Module_Surface.o Module_Thermal.o Module_Thermal_IntE_Newton_Bercovici.o Module_Thermal_Skin_GFD_Bercovici.o Module_Thermal_Skin_Newton_Arrhenius.o Module_Thermal_Skin_Newton_Bercovici.o Module_Thermal_Skin_Newton_Roscoe.o Module_Thickness.o Module_Thickness_Inte_GFD_Bercovici.o Module_Thickness_Skin_Newton_Arrhenius.o Module_Thickness_Skin_Newton_Bercovici.o Module_Thickness_Skin_Newton_Roscoe.o
 rm *.o
 rm *.mod
 
