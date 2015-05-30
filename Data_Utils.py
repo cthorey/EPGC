2c6f3b24db6c362340efcf03e788a671bc66e6a6
Cthorey_Malbec <clement.thorey@gmail.com>
2015-05-30 17:34:47 +0200

    m

--- Data_Utils.py
+++ Data_Utils.py
@@ -175,8 +175,8 @@ def copy_folder(src,dest):
 
 root_path = Who_is_Root()
 runs = []
-runs.append(os.path.join('ELAS','MSkin_TSc_Newton_HSc_Newton_RArrhenius','Run_2015-05-30_1'))
-runs.append(os.path.join('ELAS','MSkin_TSc_Newton_HSc_Newton_RArrhenius','Run_2015-05-30_2'))
+# runs.append(os.path.join('ELAS','MSkin_TSc_Newton_HSc_Newton_RArrhenius','Run_2015-05-30_1'))
+runs.append(os.path.join('ELAS','MSkin_TSc_Newton_HSc_Newton_RArrhenius','Run_2015-05-30_3'))
 # runs.append(os.path.join('ELAS','MSkin_TSc_Newton_HSc_Newton_RBercovici','Run_2015-05-30_0'))
 # runs.append(os.path.join('GRAV','MSkin_TSc_GFD_HSc_Newton_RArrhenius','Run_2015-05-30_0'))
 # runs.append(os.path.join('GRAV','MSkin_TSc_GFD_HSc_Newton_RBercovici','Run_2015-05-30_0'))
