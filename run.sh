08200f4378d7ce906cc72257c493b6df89b6c0ea
cthorey <clement.thorey@gmail.com>
2015-06-23 14:46:39 +0200

    updata

--- run.sh
+++ run.sh
@@ -9,6 +9,7 @@ Compilateur -o Module_Init_tmp.o -c Module_Init_tmp.f90
 Compilateur -o Module_Mobility.o -c Module_Mobility.f90
 Compilateur -o Module_Output.o -c Module_Output.f90
 Compilateur -o Module_Surface.o -c Module_Surface.f90
+Compilateur -o Module_Thermal_GFD_GRAVI_Arrhenius.o -c Module_Thermal_GFD_GRAVI_Arrhenius.f90
 Compilateur -o Module_Thermal_GFD_GRAVI_Bercovici.o -c Module_Thermal_GFD_GRAVI_Bercovici.f90
 Compilateur -o Module_Thermal_IntE_Newton_Bercovici.o -c Module_Thermal_IntE_Newton_Bercovici.f90
 Compilateur -o Module_Thermal_Skin_GFD_Bercovici.o -c Module_Thermal_Skin_GFD_Bercovici.f90
@@ -19,6 +20,7 @@ Compilateur -o Module_Thermal_Skin_Newton_Bercovici.o -c Module_Thermal_Skin_New
 Compilateur -o Module_Thermal_Skin_Newton_Roscoe.o -c Module_Thermal_Skin_Newton_Roscoe.f90
 Compilateur -o Module_Thermal.o -c Module_Thermal.f90
 Compilateur -o Module_Thickness_Inte_GFD_Bercovici.o -c Module_Thickness_Inte_GFD_Bercovici.f90
+Compilateur -o Module_Thickness_GRAV_Arrhenius.o -c Module_Thickness_GRAV_Arrhenius.f90
 Compilateur -o Module_Thickness_GRAV_Bercovici.o -c Module_Thickness_GRAV_Bercovici.f90
 Compilateur -o Module_Thickness_Skin_Newton.o -c Module_Thickness_Skin_Newton.f90
 Compilateur -o Module_Thickness_Skin_Newton_Arrhenius.o -c Module_Thickness_Skin_Newton_Arrhenius.f90
@@ -28,8 +30,7 @@ Compilateur -o Module_Thickness.o -c Module_Thickness.f90
 Compilateur -o Module_Thickness_Skin_Newton_Roscoe.o -c Module_Thickness_Skin_Newton_Roscoe.f90
 Compilateur -o lib_array.o -c lib_array.f90
 Compilateur -o main.o -c main.f90
-Compilateur -o run lib_array.o main.o Mobility_Thermal_Skin_Rheology.o Mobility_Thickness_Skin_Rheology.o Module_Complementaire.o Module_Conservation.o Module_Init_tmp.o Module_Mobility.o Module_Numerical_Integration.o Module_Output.o Module_Surface.o Module_Thermal.o Module_Thermal_GFD_GRAVI_Bercovici.o Module_Thermal_IntE_Newton_Bercovici.o Module_Thermal_Skin_GFD_Bercovici.o Module_Thermal_Skin_GFD_GRAVI.o Module_Thermal_Skin_Newton.o Module_Thermal_Skin_Newton_Arrhenius.o Module_Thermal_Skin_Newton_Bercovici.o Module_Thermal_Skin_Newton_Roscoe.o Module_Thickness.o Module_Thickness_GRAV_Bercovici.o Module_Thickness_Inte_GFD_Bercovici.o Module_Thickness_Newton_Gravi.o Module_Thickness_Skin_Newton.o Module_Thickness_Skin_Newton_Arrhenius.o Module_Thickness_Skin_Newton_Bercovici.o Module_Thickness_Skin_Newton_Roscoe.o
-
+Compilateur -o run lib_array.o main.o Mobility_Thermal_Skin_Rheology.o Mobility_Thickness_Skin_Rheology.o Module_Complementaire.o Module_Conservation.o Module_Init_tmp.o Module_Mobility.o Module_Numerical_Integration.o Module_Output.o Module_Surface.o Module_Thermal.o Module_Thermal_GFD_GRAVI_Arrhenius.o Module_Thermal_GFD_GRAVI_Bercovici.o Module_Thermal_IntE_Newton_Bercovici.o Module_Thermal_Skin_GFD_Bercovici.o Module_Thermal_Skin_GFD_GRAVI.o Module_Thermal_Skin_Newton.o Module_Thermal_Skin_Newton_Arrhenius.o Module_Thermal_Skin_Newton_Bercovici.o Module_Thermal_Skin_Newton_Roscoe.o Module_Thickness.o Module_Thickness_GRAV_Arrhenius.o Module_Thickness_GRAV_Bercovici.o Module_Thickness_Inte_GFD_Bercovici.o Module_Thickness_Newton_Gravi.o Module_Thickness_Skin_Newton.o Module_Thickness_Skin_Newton_Arrhenius.o Module_Thickness_Skin_Newton_Bercovici.o Module_Thickness_Skin_Newton_Roscoe.o
 rm *.o
 rm *.mod
 
